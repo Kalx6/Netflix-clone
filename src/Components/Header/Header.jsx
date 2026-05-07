8814b86aea391b822a51a17cba78a35726cbfa70
Kalx6 <kalidabdu921@gmail.com>
2026-05-07 21:16:38 +0300

    initail setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        {/* Logo */}
+        <img src="" alt="" />
+        
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
