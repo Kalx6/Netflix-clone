13942dfca483331454fe4c99261d5730f98ae62f
Kalx6 <kalidabdu921@gmail.com>
2026-05-13 22:54:56 +0300

    style: style the movie card

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -2,36 +2,63 @@ import React from "react";
 import styles from "./MovieCard.module.css";
 import { FaCirclePlay } from "react-icons/fa6";
 import { BsPlusCircle } from "react-icons/bs";
-import { GoCheckCircle } from "react-icons/go";
+import { GoCheckCircleFill } from "react-icons/go";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 function MovieCard(movie) {
   let gener = ["Adventure", "Action", "Thriller"];
   return (
-    <div>
-      <img src={movie.poster_path} alt="" />
+    <div className={styles.movieCardWrapper}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt="poster image"
+      />
 
-      <div>
-        <img src={movie.poster_path} alt="" />
-        <div>Recently Added </div>
-        <div>
-          <FaCirclePlay color="white" size={40} />
-          <BsPlusCircle color="white" size={40} />
-          <GoCheckCircle color="white" size={40} />
-          <IoIosArrowDropdownCircle color="white" size={40} />
+      <div className={styles.hoverCard}>
+        <img
+          src={movie.poster_path}
+          className={styles.hoverImage}
+          alt="hover image"
+        />
+        <div className={styles.badge}>Recently Added </div>
+        <div className={styles.buttonRow}>
+          <FaCirclePlay
+            color="white"
+            size={40}
+            className={styles.circleButton}
+          />
+          <BsPlusCircle
+            color="white"
+            size={40}
+            className={styles.circleButton}
+          />
+          <GoCheckCircleFill
+            color="white"
+            size={40}
+            className={styles.circleButton}
+          />
+          <IoIosArrowDropdownCircle
+            className={styles.circleButtonSmall}
+            color="white"
+            size={40}
+          />
         </div>
 
-        <div>
+        <div className={styles.metaRow}>
           <span>U/A 16+</span>
           <span>Movie</span>
           <span>HD</span>
         </div>
 
-        <div>
+        <div className={styles.gener}>
           {gener.map((a, index) => (
             <span key={index}>
               {a}
-              {index < gener.length - 1 && <span>•</span>}
+              <span> </span>
+              {index < gener.length - 1 && (
+                <span className={styles.dot}>•</span>
+              )}
             </span>
           ))}
         </div>
