e9d35173b1882858f50b321e56510e37244d5360
Kalx6 <kalidabdu921@gmail.com>
2026-05-21 09:22:10 +0300

    modify: modify the banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -23,7 +23,7 @@ function Banner() {
   }, []);
 
   function truncate(str, n) {
-    return str?.length > n ? str.substr(0, n - 1) : str;
+    return str?.length > n ? str.substr(0, n - 1)+"..." : str;
   }
   return (
     <div
