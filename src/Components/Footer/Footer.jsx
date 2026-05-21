79908a5dbb48dbd0896fec5efdc44bef4cdc1bba
Kalx6 <kalidabdu921@gmail.com>
2026-05-21 09:20:10 +0300

    create footer component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        {/* Social Icons */}
+        <div className={styles.socialIcons}>
+          <FaFacebookF className={styles.icon} />
+          <FaInstagram className={styles.icon} />
+          <FaTwitter className={styles.icon} />
+          <FaYoutube className={styles.icon} />
+        </div>
+
+        {/* Footer Links */}
+        <div className={styles.linksContainer}>
+          <div className={styles.column}>
+            <a href="/">Audio Description</a>
+            <a href="/">Investor Relations</a>
+            <a href="/">Legal Notices</a>
+            <a href="/">Netflix Originals</a>
+          </div>
+
+          <div className={styles.column}>
+            <a href="/">Help Centre</a>
+            <a href="/">Jobs</a>
+            <a href="/">Cookie Preferences</a>
+            <a href="/">Account</a>
+          </div>
+
+          <div className={styles.column}>
+            <a href="/">Gift Cards</a>
+            <a href="/">Terms of Use</a>
+            <a href="/">Corporate Information</a>
+            <a href="/">Ways to Watch</a>
+          </div>
+
+          <div className={styles.column}>
+            <a href="/">Media Centre</a>
+            <a href="/">Privacy</a>
+            <a href="/">Contact Us</a>
+            <a href="/">Speed Test</a>
+          </div>
+        </div>
+
+        {/* Copyright */}
+        <p className={styles.copy}>
+          © 1997-2026 Netflix, Inc.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
